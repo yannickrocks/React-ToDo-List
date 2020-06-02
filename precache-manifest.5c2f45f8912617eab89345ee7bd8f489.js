4e55d31a7123e276cd4d48336c0ff99832215971
Yannick Mermet <yannickmermet@hotmail.com>
2020-06-02 11:17:28 +0100

    Updates

---
+++ precache-manifest.5c2f45f8912617eab89345ee7bd8f489.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8a8a32dd4d6e84f68ecc8ddb5e2c95ca",
+    "url": "/ReactToDoList/index.html"
+  },
+  {
+    "revision": "649c42c0e8321f719268",
+    "url": "/ReactToDoList/static/css/main.ee3327b1.chunk.css"
+  },
+  {
+    "revision": "a18b61adbb7055d13c71",
+    "url": "/ReactToDoList/static/js/2.0d8ef751.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/ReactToDoList/static/js/2.0d8ef751.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "649c42c0e8321f719268",
+    "url": "/ReactToDoList/static/js/main.7a827037.chunk.js"
+  },
+  {
+    "revision": "25002223d2a2e769482b",
+    "url": "/ReactToDoList/static/js/runtime-main.302ca297.js"
+  },
+  {
+    "revision": "50294de8cb52f7114d015739bb6dfeb9",
+    "url": "/ReactToDoList/static/media/checkbox.50294de8.svg"
+  },
+  {
+    "revision": "406450f3248f993f1c3a50f13b3eddf2",
+    "url": "/ReactToDoList/static/media/done.406450f3.svg"
+  },
+  {
+    "revision": "29cca656acede59a21c097243771a538",
+    "url": "/ReactToDoList/static/media/trash-can.29cca656.svg"
+  }
+]);
